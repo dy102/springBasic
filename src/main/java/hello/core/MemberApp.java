37e8adeff8b5447c40c06a3da6846c58e4ed120d
dy102 <ekagnlek2@gmail.com>
2023-12-15 22:01:38 +0900

    관심사의 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,11 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {//한계가 있는 테스트 방식. junit을 사용하자.
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {//Impl: 구현체가 하나만 있을 때 대개 쓰는 용어
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //추상화에도 의존, 구체화에도 의존 -> DIP원칙을 위반함.
+    private final MemberRepository memberRepository; //DIP 준수
+
+    public MemberServiceImpl(MemberRepository memberRepository) { // 추상화에만 의존. 구체적인건 모름
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
