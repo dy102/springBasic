db96a880590710ae3b84cb163127c7c74996462b
dy102 <ekagnlek2@gmail.com>
2023-12-20 19:26:52 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+    public MemberService memberService() { // MemberService 역할이 드러남
+        return new MemberServiceImpl(memberRepository()); //저장소 역할이 잘 보이지 않음.
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {// MemberRepository 역할이 드러남
+        return new MemoryMemberRepository(); //다른 저장소가 필요한 경우, 이 코드만 바꾸면 된다.
+    }
+
+    public OrderService orderService() {// OrderService 역할이 드러남
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
