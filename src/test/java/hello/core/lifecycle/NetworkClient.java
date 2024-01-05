e4210ec294dd724b68e8edd1872e8b6ebf75e265
dy102 <ekagnlek2@gmail.com>
2024-01-05 19:12:13 +0900

    s8: 빈 생명주기 콜백 시작

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");//메시지를 보냄
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    //실제 콜 연결이 된 상태에서 콜을 부름
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출 -> 안전하게 서비스 연결이 끊어짐.
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
